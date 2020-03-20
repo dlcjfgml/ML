f367852483e848f484e6bcef4e601ed189ad67f4
lee <wpdlwpdl0@gmail.com>
2020-03-20 21:05:46 +0900

    first

---
+++ mvmul/.bash_history
@@ -0,0 +1,49 @@
+ls
+sudo passwd root
+passwd root
+ls
+cd src
+ls
+vim fpga_api.cpp 
+vim py_lib.cpp 
+cd ..
+ls
+cd src
+ls
+vim fpga_api.cpp 
+s[Bh download.sh
+cd ..
+sh download.sh 
+ls
+ls
+cd data
+ls
+cd ..
+ls
+vim Makefile 
+cd data
+ls
+c d..
+cd ..
+ls
+cd proto/
+ls
+vim caffe.proto 
+cd ..
+cd pretrained_weights/
+ls
+cd ..
+ls
+cd include/
+ls
+vim fpga_api.h 
+vim digit_dnn.h 
+cd py_lib.h 
+vim py_lib.h 
+cd ..
+ls
+cd src
+ls
+vim digit_dnn.cpp 
+ls
+vim fpga_api.cpp 
---
+++ mvmul/data/load_mnist.py
@@ -0,0 +1,44 @@
+import os, struct
+import numpy as np
+from array import array
+
+def load_mnist(mode="train", digits=np.arange(10), path="."):
+    """
+    Loads MNIST files into 3D numpy arrays
+
+    Adapted from: http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
+    """
+
+    if mode == "train":
+        fname_img = os.path.join(path, 'train-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
+    elif mode == "test":
+        fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
+    else:
+        raise ValueError("mode must be 'train' or 'test'")
+
+    #load test or train image
+    flbl = open(fname_lbl, 'rb')
+    # read 8byte and II means first size 4 (unsigned int) to magic_nr, next to size
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = array("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = array("B", fimg.read())
+    fimg.close()
+
+    #convert image
+    ind = [ k for k in range(size) if lbl[k] in digits ]
+    N = len(ind)
+
+    images = np.zeros((N, rows, cols), dtype=np.uint8)
+    labels = np.zeros((N, 1), dtype=np.int8)
+    for i in range(len(ind)):
+        images[i] = np.array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
+        labels[i] = lbl[ind[i]]
+
+    labels = labels.reshape((N,))
+    return images, labels
